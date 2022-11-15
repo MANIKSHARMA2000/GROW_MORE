cb7040326ffb527744595a11ff26cdf17aabe028
MANIKSHARMA2000 <115614705+MANIKSHARMA2000@users.noreply.github.com>
2022-11-15 15:05:26 +0530

    version 1 completed

--- stocks-GM.js
+++ stocks-GM.js
@@ -3,18 +3,29 @@ let ulEl = document.getElementById('ul-el')
 let AddBtn = document.getElementById('add-btn')
 let Listitem = document.getElementById('listitem')
 let RenderWatchlist = document.getElementById("render-watchlist")
-
-
+let ItemWatchlist = document.getElementById("item-watchlist")
 let listboxEl = document.getElementById("listbox")
 let NSEbtn = document.getElementById("nse-btn")
 let NSDQbtn = document.getElementById("nsdq-btn")
 let BNBSbtn = document.getElementById("bnbs-btn")
-let exchange = 0
+
+
+let Table = document.getElementById("table")
+let DBtn = document.getElementById("d-btn")
+let orderbox = document.getElementById("main-2-two-1")
 let usStocks 
 let nsestocks
 let Cryptonames
 let NseWholeData
+let cryptoWholeData
 let usStockpriceData 
+let exchange = 0
+let buyingticker
+let buyingtickerPrice
+let buyingtickerExchange
+let TotalorderNumber = document.getElementById("total-orders")
+let Totalorders = 0
+
 
 
 // import data for nsdaq
@@ -22,8 +33,6 @@ fetch("us-data.json").then(response =>response.json())
 .then(data => {console.log(data)
     usStocks = data
 });
-
-
 //fetch data for NSE
 const options = {
 	method: 'GET',
@@ -37,31 +46,138 @@ fetch('https://latest-stock-price.p.rapidapi.com/any', options)
 .then(data => {console.log(data);
     NseWholeData = data
     console.log(NseWholeData[1].previousClose);
-    
     nsestocks = data.map(data=>data.symbol)})
-
 //fetch data for crypto coins
-fetch('https://api.coincap.io/v2/assets')
-.then(response =>response.json()).then(content => {console.log(content.data)
-    Cryptonames =  content.data.map(content => content.name)
-console.log(Cryptonames);})
+fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=true')
+.then(response =>response.json()).then(data => {console.log(data)
+    cryptoWholeData = data
+    Cryptonames =  data.map(content => content.name)
+})
+
+NSEbtn.addEventListener("click",()=>{exchange = 1
+    NSEbtn.style.backgroundColor = "#353839"
+    NSDQbtn.style.backgroundColor = "whitesmoke"
+    BNBSbtn.style.backgroundColor = "whitesmoke"
+Renderlist()})
+NSDQbtn.addEventListener("click",()=>{exchange = 2
+    NSEbtn.style.backgroundColor = "whitesmoke"
+    NSDQbtn.style.backgroundColor = "#353839"
+    BNBSbtn.style.backgroundColor = "whitesmoke"
+Renderlist()})
+BNBSbtn.addEventListener("click",()=>{exchange = 3
+    NSEbtn.style.backgroundColor = "whitesmoke"
+    NSDQbtn.style.backgroundColor = "whitesmoke"
+    BNBSbtn.style.backgroundColor = "#353839"
+Renderlist()})
+
+function closePannel(){
+    orderbox.innerHTML = ""
+}
+ function CreateOrder(){
+   orderbox.innerHTML = `<div id= card>
+   <div id="buy-info-close">
+         <div class="buy-info">
+            <p>Buy <span id="black">${buyingticker}</span>  - <span id="black">${buyingtickerPrice}</span> </p> 
+            <p>EXCHANGE - <span id="black">${buyingtickerExchange}</span></p>
+         </div>
+        <div class="closeButton">
+            <button onclick="closePannel()" id="Close-Btn"><i class="fa-solid fa-xmark fa-2xl"></i></button>
+        </div>
+    </div>
+<div class="order-details">
+    <p>Order Type:</p>
+     <a href=""><input id="r-btn" type="radio">Regular</a>
+     <a href=""><input id="r-btn" type="radio">Cover</a>
+</div>
+<div class="details">
+    <fieldset class="field">
+        <input value=${buyingtickerPrice} id="input-price" type="number"><legend>Price</legend>
+    </fieldset>
+    <fieldset class="field">
+        <input value ="1" id="input-qty" type="number"><legend>QTY:</legend>
+    </fieldset>
+    
+</div>
+<div class="radiobtn">
+    <p> <input type="radio"> Limit</p>
+    <p> <input type="radio"> Market</p>
+</div>
+<div class="buy-sell">
+    <p>Margin Required - <span id="black">${buyingtickerPrice}</span></p>
+    <button onclick="BuySellStock()" id="buy-btn">BUY</button>
+    <button onclick="BuySellStock()" id="sell-btn">SELL</button>
+</div>
+</div>`
 
 
-NSEbtn.addEventListener("click",()=>{exchange = 1})
-NSDQbtn.addEventListener("click",()=>{exchange = 2})
-BNBSbtn.addEventListener("click",()=>{exchange = 3})
- 
-function UpdateWatchlist(str1,str2,str3){
-    RenderWatchlist.innerHTML += `<div id="item-watchlist">
-    <p>${str1}  ${str2}  ${str3}</p>
-    <div id="btns">
-        <button>B</button>
-        <button>S</button>
-        <button >C</button>
+}
+function done(){
+    orderbox.innerHTML = ""
+}
+ function BuySellStock(){
+    Totalorders++
+    TotalorderNumber.innerHTML = `${Totalorders}`
+    orderbox.innerHTML = `<div id="orderplacingcard">
+    <div id="order-card-info">
+        <p>
+        Your Order Request Has Been Sent It Will Be Executed In The Next Trading Session Thank You! </p>
+    </div>
+    <div class="okay-btn">
+        <button onclick="done()" id="okay-btn">Confirm</button>
+    </div>
+</div>`
+ }
+function UpdateWatchlist(exchange,str1,str2,str3){
+    if(str3>0){
+        RenderWatchlist.innerHTML += `<div class="item-watchlist"  id="${str1}">
+        <div>
+    <span class="green">${str1}</span><span id="item-exchangeVal">${exchange}</span>
+    <span><i class="fa-solid fa-chevron-up"></i> ${str3.toFixed(2)} %</span>
+    <span class="green">${str2}</span>
+    </div>
+    <div  id="item-btns">
+        <button id="b-btn" onclick="CreateOrder()">B</button>
+        <button id="s-btn" onclick="CreateOrder()">S</button>
+        <a target="_blank" href="https://in.tradingview.com/symbols/${str1}/"><button id="c-btn" ><i class="fa-solid fa-chart-line"></i></button></a>
+        <button onclick="RemoveItemWatchlist()" id="d-btn" ><i class="fa-solid fa-trash"></i></button>
     </div>
    </div>`
-  
+   buyingticker = str1;
+   buyingtickerPrice = str2
+   buyingtickerExchange = exchange
+    }
+    if(str3<0){
+        RenderWatchlist.innerHTML += `<div class="item-watchlist" id="${str1}">
+        <div>
+    <span class="red">${str1}</span>
+    <span><i class="fa-sharp fa-solid fa-chevron-down"></i>${str3.toFixed(2)} %</span>
+    <span class="red">${str2}</span>
+    </div>
+    <div  id="item-btns">
+        <button id="b-btn" onclick="CreateOrder()">B</button>
+        <button id="s-btn" onclick="CreateOrder()">S</button>
+        <a target="_blank" href="https://in.tradingview.com/symbols/${str1}/"><button id="c-btn" ><i class="fa-solid fa-chart-line"></i></button></a>
+        <button onclick="RemoveItemWatchlist()" id="d-btn" ><i class="fa-solid fa-trash"></i></button>
+    </div>
+   </div>`
+   buyingticker = str1;
+   buyingtickerPrice = str2
+   buyingtickerExchange = exchange
+
+    }
+    
 }
+function RemoveItemWatchlist(){
+    RenderWatchlist.addEventListener('click',(e)=>{
+       let o =  e.path[3].firstElementChild.firstElementChild.textContent
+       console.log(o);
+        if(RenderWatchlist.innerHTML!=""){
+            const w = document.getElementById(o)
+            w.remove() 
+    }
+    })
+}
+
 function filterfunction(){
     let filter = myinputEl.value.toUpperCase();
     
@@ -79,7 +195,7 @@ function filterfunction(){
     }
 }
 function Renderlist(){
-    //US STOCKS
+    //US STOCK
     if(exchange===2){
     let UsStocknames
     for(let i=0; i<usStocks.length; i++){
@@ -109,6 +225,7 @@ function Renderlist(){
 myinputEl.addEventListener("click",Renderlist)
 
 ulEl.addEventListener('click',(e)=>{
+    
     console.log(e.path[0].innerHTML);
     sname = e.path[0].innerHTML
     //for nse
@@ -121,7 +238,7 @@ ulEl.addEventListener('click',(e)=>{
             pchange = NseWholeData[i].pChange
         }
     }
-    UpdateWatchlist(sname,previousclose,pchange)
+    UpdateWatchlist("NSE",sname,previousclose,pchange)
     }
     //for nsdq
     if(exchange ===2){
@@ -133,17 +250,18 @@ ulEl.addEventListener('click',(e)=>{
                  smbl = usStocks[i].Symbol
             }}
             fetch(`https://finnhub.io/api/v1/quote?symbol=${smbl}&token=cdmujmiad3i9q6h6852gcdmujmiad3i9q6h68530`).then(response=>response.json()).then(data=>{console.log(data)
-            pc = data.pc
+            pc = data.c
             percentChange = data.dp
-        }).then(setTimeout(()=>UpdateWatchlist(smbl,pc,percentChange),1000))
+        }).then(setTimeout(()=>UpdateWatchlist("NSDAQ",smbl,pc,percentChange),1000))
     }
     //for crypto
     if(exchange ===3){
-
-        fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=true`).then(response=>response.json()).then(data=>console.log(data))
+        for(let i=0; i<cryptoWholeData.length; i++){
+            if(sname === cryptoWholeData[i].name ){
+                 UpdateWatchlist("BNB",cryptoWholeData[i].symbol.toUpperCase(),cryptoWholeData[i].current_price,cryptoWholeData[i].price_change_24h)
+            }}
     }
-    
-    
+    ulEl.innerHTML = ""
 })
 listboxEl.addEventListener("mouseleave",()=>{
     ulEl.innerHTML = ''
